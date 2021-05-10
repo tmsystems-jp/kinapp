65c2440fdf77a6107f9f681b5636a590312dde5f
SatokoMatsuo <57399526+SatokoMatsuo@users.noreply.github.com>
2021-05-10 16:14:37 +0900

    db登録処理修正中

--- store/db.js
+++ store/db.js
@@ -3,8 +3,10 @@ import { db } from "~/plugins/firebase";
 export const actions = {
   async pullDefault({ app, commit, rootGetters }) {
     var user = rootGetters["userInfo"];
-    user["principal"] = "Default";
-    var path = this.test(user, "");
+    user = { principal: "Default" };
+    console.log(user);
+    var path = test(user, "");
+    console.log("★");
     console.log(path);
     // var setData = {};
     // await Promise.all(
@@ -54,67 +56,67 @@ export const actions = {
       });
   },
   async insert({ commit }, payload) {
-    var user = rootGetters["userInfo"];
-    const dbRef = payload.db;
-    await dbRef
-      .get()
-      .then((res) => {
-        commit("setUserInfo", res.data(), { root: true });
-      })
-      .catch((error) => {
-        console.log("error : " + error);
-      });
-  },
-  async test(user, num) {
-    var path = [
-      {
-        value: {
-          dbpath: db.collection("principal").doc(user.principal),
-          name: "principal"
-        }
-      }
-    ];
-    return path;
+    // var user = rootGetters["userInfo"];
+    // const dbRef = payload.db;
+    // await dbRef
+    //   .get()
+    //   .then((res) => {
+    //     commit("setUserInfo", res.data(), { root: true });
+    //   })
+    //   .catch((error) => {
+    //     console.log("error : " + error);
+    //   });
   }
+  // async test(user, num) {
+  //   var path = [
+  //     {
+  //       value: {
+  //         dbpath: db.collection("principal").doc(user.principal),
+  //         name: "principal"
+  //       }
+  //     }
+  //   ];
+  //   return path;
+  // }
 };
 
-// function test(user, num) {
-//   var path = [
-//     {
-//       key: "1",
-//       value: {
-//         dbpath: db.collection("principal").doc(user.principal),
-//         name: "principal"
-//       }
-//     }
-//     // {
-//     //   dbpath: db
-//     //     .collection("principal")
-//     //     .doc(user.principal)
-//     //     .collection("staff")
-//     //     .doc(user.staff),
-//     //   name: "staff"
-//     // },
-//     // {
-//     //   dbpath: db
-//     //     .collection("principal")
-//     //     .doc("Default")
-//     //     .collection("item")
-//     //     .doc("Default"),
-//     //   name: "item",
-//     //   set: "sub"
-//     // },
-//     // { dbpath: db.collection("parent").doc("Default"), name: "parent" },
-//     // { dbpath: db.collection("children").doc("Default"), name: "children" },
-//     // {
-//     //   dbpath: db.collection("item").doc("Default"),
-//     //   name: "item",
-//     //   set: "main"
-//     // }
-//   ];
-//   var value = path;
-//   if (num !== "") {
-//     value = path.find((z) => (z.key = num)).value;
-//   }
-//   return value;
-// }
+function test(user, num) {
+  var path = [
+    {
+      key: "1",
+      value: {
+        dbpath: db.collection("principal").doc(user.principal),
+        name: "principal"
+      }
+    }
+    // {
+    //   dbpath: db
+    //     .collection("principal")
+    //     .doc(user.principal)
+    //     .collection("staff")
+    //     .doc(user.staff),
+    //   name: "staff"
+    // },
+    // {
+    //   dbpath: db
+    //     .collection("principal")
+    //     .doc("Default")
+    //     .collection("item")
+    //     .doc("Default"),
+    //   name: "item",
+    //   set: "sub"
+    // },
+    // { dbpath: db.collection("parent").doc("Default"), name: "parent" },
+    // { dbpath: db.collection("children").doc("Default"), name: "children" },
+    // {
+    //   dbpath: db.collection("item").doc("Default"),
+    //   name: "item",
+    //   set: "main"
+    // }
+  ];
+  var value = path;
+  if (num !== "") {
+    value = path.find((z) => (z.key = num)).value;
+  }
+  return value;
+}
