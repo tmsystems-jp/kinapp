8a5e3f31ecbace1f8ffee3563f652301fed5ef02
Yasuharu Nishida <yasuharu.nishida@tm-systems.co.jp>
2021-05-14 17:19:11 +0900

    child

--- pages/user/childrenRegist.vue
+++ pages/user/childrenRegist.vue
@@ -115,109 +115,121 @@
             <div>
               <fieldset>
                 <legend>登園方法</legend>
-                <label
-                  ><input
-                    type="radio"
-                    name="going-school"
-                    value="parents"
-                    v-model="input['going-school']"
-                  />保護者送迎</label
-                >
-                <label
-                  ><input
-                    type="radio"
-                    name="going-school"
-                    value="bus"
-                    v-model="input['going-school']"
-                  />バス利用</label
-                >
+                <div>
+                  <label v-for="data in goingSchoolItem" :key="data.key"
+                    ><input
+                      type="radio"
+                      name="going-school"
+                      :value="data.key"
+                      v-model="input['going-school']"
+                    />{{ data.value }}</label
+                  >
+                </div>
+                <span>{{ itemName("going-school") }}</span>
               </fieldset>
               <fieldset v-if="input['going-school'] === 'bus'">
                 <legend>バスコース</legend>
-                <select
-                  name="bus-course"
-                  v-model="input['bus-course']"
-                  ref="buscourse"
-                  @change="$selectChange($refs.buscourse, input['bus-course'])"
-                  data-view="off"
-                >
-                  <option value="" selected>バスコース</option>
-                  <option
-                    v-for="data in busCourse"
-                    :key="data.value"
-                    :value="data.value"
+                <div>
+                  <select
+                    name="bus-course"
+                    v-model="input['bus-course']"
+                    ref="buscourse"
+                    @change="
+                      $selectChange($refs.buscourse, input['bus-course'])
+                    "
+                    data-view="off"
                   >
-                    {{ data.name }}
-                  </option>
-                </select>
+                    <option value="" selected>バスコース</option>
+                    <option
+                      v-for="data in busCourse"
+                      :key="data.value"
+                      :value="data.value"
+                    >
+                      {{ data.name }}
+                    </option>
+                  </select>
+                </div>
+                <span>{{ itemName("bus-course") }}</span>
               </fieldset>
             </div>
             <div>
               <fieldset>
                 <legend>郵便番号</legend>
-                <input
-                  type="number"
-                  v-model="input['postno']"
-                  placeholder="郵便番号"
-                  @input="postSearch()"
-                />
+                <div>
+                  <input
+                    type="number"
+                    v-model="input['postno']"
+                    placeholder="郵便番号"
+                    @input="postSearch()"
+                  />
+                </div>
                 <span>{{ input["postno"] }}</span>
               </fieldset>
               <fieldset>
                 <legend>都道府県</legend>
-                <select
-                  v-model="input['prefectures']"
-                  ref="prefectures"
-                  @change="
-                    $selectChange($refs.prefectures, input['prefectures'])
-                  "
-                  data-view="off"
-                >
-                  <option value="" selected>都道府県</option>
-                  <option
-                    v-for="data in prefecturesList"
-                    :key="data.code"
-                    :value="data.code"
+                <div>
+                  <select
+                    v-model="input['prefectures']"
+                    ref="prefectures"
+                    @change="
+                      $selectChange($refs.prefectures, input['prefectures'])
+                    "
+                    data-view="off"
                   >
-                    {{ data.name }}
-                  </option>
-                </select>
+                    <option value="" selected>都道府県</option>
+                    <option
+                      v-for="data in prefecturesList"
+                      :key="data.code"
+                      :value="data.code"
+                    >
+                      {{ data.name }}
+                    </option>
+                  </select>
+                </div>
                 <span>{{ input["prefectures"] }}</span>
               </fieldset>
               <fieldset>
                 <legend>区市町村</legend>
-                <input
-                  type="text"
-                  v-model="input['address1']"
-                  placeholder="区市町村"
-                />
+                <div>
+                  <input
+                    type="text"
+                    v-model="input['address1']"
+                    placeholder="区市町村"
+                  />
+                </div>
                 <span>{{ input["address1"] }}</span>
               </fieldset>
               <fieldset>
                 <legend>丁目・番地</legend>
-                <input
-                  type="text"
-                  v-model="input['address2']"
-                  placeholder="丁目・番地"
-                />
+                <div>
+                  <input
+                    type="text"
+                    v-model="input['address2']"
+                    placeholder="丁目・番地"
+                  />
+                </div>
                 <span>{{ input["address2"] }}</span>
               </fieldset>
               <fieldset>
                 <legend>以降・建物名</legend>
-                <input
-                  type="text"
-                  v-model="input['address3']"
-                  placeholder="建物名"
-                />
+                <div>
+                  <input
+                    type="text"
+                    v-model="input['address3']"
+                    placeholder="建物名"
+                  />
+                </div>
                 <span>{{ input["address3"] }}</span>
               </fieldset>
               <fieldset>
                 <legend>電話番号</legend>
-                <input
-                  type="tel"
-                  v-model="input['telephone']"
-                  placeholder="電話番号"
-                />
+                <div>
+                  <input
+                    type="tel"
+                    v-model="input['telephone']"
+                    placeholder="電話番号"
+                  />
+                </div>
                 <span>{{ input["telephone"] }}</span>
               </fieldset>
               <fieldset
@@ -231,12 +243,14 @@
                     )
                   }}
                 </legend>
-                <input
-                  type="tel"
-                  v-model="input['emergency-contact'][index]"
-                  placeholder="緊急連絡先"
-                  @input="emergencyContact()"
-                />
+                <div>
+                  <input
+                    type="tel"
+                    v-model="input['emergency-contact'][index]"
+                    placeholder="緊急連絡先"
+                    @input="emergencyContact()"
+                  />
+                </div>
                 <span>{{ input["emergency-contact"][index] }}</span>
               </fieldset>
             </div>
@@ -340,6 +354,10 @@ export default {
       input: this.$ifchildren(),
       prefecturesList: this.$prefecturesList(),
       busCourse: [],
+      goingSchoolItem: [
+        { key: "parents", value: "保護者送迎" },
+        { key: "bus", value: "バス利用" },
+      ],
     };
   },
   methods: {
@@ -348,6 +366,15 @@ export default {
         this.status = "edit";
       } else {
         this.status = "off";
+      }
+    },
+    itemName(name) {
+      if (name === "going-school") {
+        if (this.input["going-school"] !== "") {
+          return this.goingSchoolItem.find(
+            (z) => z.key === this.input["going-school"]
+          ).value;
+        }
       }
     },
     emergencyContact() {
